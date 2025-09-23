da84acbd789608c6e43ab6f396cd78614bd7486c
Khanh Cung <cungkhanh05111994@gmail.com>
2025-09-23 22:25:25 +0700

    update gitignore

--- src/Order.API/obj/Debug/net9.0/Order.API.AssemblyInfo.cs
+++ src/Order.API/obj/Debug/net9.0/Order.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Order.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94ddf3ec40d020a1bac2b606cbd66a77f97f24ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Order.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Order.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
